6bd48fdec7e1964fe9fbb6cb906acfc8a561b9be
eliskimo <elismeric@gmail.com>
2018-10-27 20:35:52 +0200

    e

--- PAP/ejbModule/PAP/SERVICES/ObjectService.java
+++ PAP/ejbModule/PAP/SERVICES/ObjectService.java
@@ -1,16 +1,54 @@
 package PAP.SERVICES;
 
+import java.util.ArrayList;
+import PAP.ENTITY.*;
+import java.util.List;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("object")
+import PAP.ENTITY.ObjectPAP;
+
+@Path("/magasin")
 public class ObjectService {
 	
-	@GET
-	@Produces(MediaType.TEXT_HTML)
-	public String test() {
-		return "<h1> Ã§a marche enfin cette merde #MerceMourad</h1>";
+	private static List<ObjectPAP> produits = new ArrayList<ObjectPAP>();
+	
+	//F2
+	@POST @Path("/drop")
+	@Consumes({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+	public void  dropObj(ObjectPAP produit) {
+		//ajouter des param?
+		produits.add(produit);
+		
 	}
+	
+	//F3
+	@GET @Path("/search/{nameProd}")
+	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+	public boolean searchObj(@PathParam("nameProd")ObjectPAP name) {
+		
+		for(ObjectPAP nameProd : produits) {
+			if(produits.contains(name)){
+				return produits.contains(name);
+            } else
+            	System.out.println("L'objet recherché n'existe pas");
+	}
+	
+	//F5
+	@GET @Path("/ca")
+	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+	public int calcCA(int ca) {
+		//cest avec la bdd et je sais pas comment faire
+		//TransactionPAP t = 0;
+		//t.getPAPFeesAmount();
+		return ca;
+		
+		
+	}
+	
 }
--- PAPWeb/src/controler/dedie/CSubscribe.java
+++ PAPWeb/src/controler/dedie/CSubscribe.java
@@ -10,7 +10,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.sun.enterprise.security.jauth.callback.PrivateKeyCallback.Request;
 
 import PAP.SESSION.IApplication;
 
@@ -20,7 +19,8 @@ public class CSubscribe implements ICTreatment {
 	String name,email,pass,city;
 	
 	public CSubscribe() throws NamingException {
-		
+		ctx = new InitialContext();
+		app = (IApplication) ctx.lookup("ejb/PAP");
 		
 		//Ca a l'air de fonctionner mais nullPointerException
 		//DataSource ds = (DataSource) envCtx.lookup("ejb/PAP")
@@ -28,25 +28,27 @@ public class CSubscribe implements ICTreatment {
 	}
 	
 	@Override
-	public void treatRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	public void treatRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//We get request's attributes 
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String pass = request.getParameter("pass");
+		String city = request.getParameter("city");
 		
-		try {
-			ctx = new InitialContext();
-			app = (IApplication) ctx.lookup("ejb/PAP");
-			System.out.println(app);
-		} catch (NamingException e1) {
-			// TODO Auto-generated catch block
+		try {		
+			//We ask glassfish to suscribe the adherent
+			app.subscribe(name, email, pass, city);
+		} catch (Exception e1) {
+			request.setAttribute("erreur", e1);
 			e1.printStackTrace();
 		}
 		
-		this.name = (String) request.getParameter("name");
-		this.email = (String) request.getParameter("email");
-		this.pass = (String) request.getParameter("pass");
-		this.city = (String) request.getParameter("city");	
-		
-		//We ask glassfish for suscribe the adherent
-		app.subscribe(name, email, pass, city);
+//		this.name = (String) request.getParameter("name");
+//		this.email = (String) request.getParameter("email");
+//		this.pass = (String) request.getParameter("pass");
+//		this.city = (String) request.getParameter("city");	
+
+
 		//
 		RequestDispatcher dispatch = request.getRequestDispatcher("./View/subscribeAns.jsp");
 		dispatch.forward(request, response);
