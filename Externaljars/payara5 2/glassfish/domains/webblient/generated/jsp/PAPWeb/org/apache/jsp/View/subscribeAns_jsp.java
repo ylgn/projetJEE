b05c085bbdf57bedac1ce1e2e3949f11b9a53c8e
Yannisleguen <yannis.belkebir@etu.parisdescartes.fr>
2018-10-27 13:32:33 +0200

    End of add external jars

---
+++ Externaljars/payara5 2/glassfish/domains/webblient/generated/jsp/PAPWeb/org/apache/jsp/View/subscribeAns_jsp.java
@@ -0,0 +1,93 @@
+package org.apache.jsp.View;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class subscribeAns_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html; charset=ISO-8859-1");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("\n");
+
+	Exception erreurDeCreationCompte = (Exception) request.getAttribute("erreur");	
+	String nomClient = request.getParameter("name");
+	String emailClient = request.getParameter("email");
+	String passClient = request.getParameter("pass");
+	String villeClient = request.getParameter("city");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<html>\n");
+      out.write("<head>\n");
+      out.write("<meta charset=\"ISO-8859-1\">\n");
+      out.write("<title>Insert title here</title>\n");
+      out.write("</head>\n");
+      out.write("<body>\n");
+      out.write("\t<h1> Résultat de demande de création de compte </h1>\n");
+      out.write("\t<h2>\n");
+      out.write("\t\t");
+ if (erreurDeCreationCompte == null) { 
+      out.write("\n");
+      out.write("\t\t\t<span style=\"color: red;\"> Compte créé pour ");
+      out.print( nomClient );
+      out.write(" </span> !\n");
+      out.write("\t\t");
+}	else { 
+      out.write("\n");
+      out.write("\t\t\tLe compte <span style=\"color: red;\">  n'a pas été créé !!! </span>\n");
+      out.write("\t\t");
+} 
+      out.write("\n");
+      out.write("\t</h2>\n");
+      out.write("</body>\n");
+      out.write("</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
